8cbbce2fbf9e8d0b0e97eb732f091a04dc4a54e2
Pradeep Kumar <webreinvent@gmail.com>
2019-04-05 01:29:38 +0530

    first commit

---
+++ app/components/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*" rows="*">
+
+
+            <Label class="message" :text="msg" col="0" row="0"/>
+        </GridLayout>
+
+        <StackLayout>
+            <Button text="To Details directly" @tap="$navigateTo(detailPage)" />
+        </StackLayout>
+
+    </Page>
+</template>
+
+<script >
+    import DetailPage from './DetailPage'
+  export default {
+    data() {
+      return {
+          msg: 'Hello World!',
+          detailPage: DetailPage
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ app/components/DetailPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <Page>
+        <ActionBar title="Detail"/>
+        <StackLayout>
+            <Label text="Details.." />
+        </StackLayout>
+    </Page>
+</template>
+
+<script >
+    export default {
+        data() {
+            return {
+                msg: 'Hello World!',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
